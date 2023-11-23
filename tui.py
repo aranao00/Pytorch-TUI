b9fca3c8f9aaaa483661fc39e13905f9525dec01
aranao00 <150121196+aranao00@users.noreply.github.com>
2023-11-23 16:32:29 +0900

    Add files via upload

--- tui.py
+++ tui.py
@@ -4,6 +4,8 @@ import json
 import importlib
 import re
 import model_architectures#사용자가 제작한 모델들의 실제 class가 저장된 곳
+import trainloops
+import custom_loss
 importlib.reload(model_architectures)
 '''
 error code list:
@@ -15,14 +17,10 @@ error code list:
 
 basecode='import torch\nimport torch.nn\nimport torch.nn as nn\nimport torch.optim as optim\n'
 
-def find_index_containing_text(text_list, search_text):
-    for index, text in enumerate(text_list):
-        if search_text in text:
-            return index
-    return -1  # 해당 텍스트를 포함하는 문자열을 찾지 못한 경우 -1 반환
-
 class Architecture_Database():#아키텍처별로 코드 따로 저장하게 나중에 바꿔보기. 그리고 가중치 유지한채로 코드 바꾸는것도 지원하게.
 #+모델 순서나 concate등의 사용도 자유롭게 설정할 수 있게 .
+#custom train loop 추가되었으니 업데이트할것.
+#custom loss 추가되었으니 업데이트 할 것.
     def __init__(self):
         #아키텍처는 사용자가 해당 모델의 구조를 확인하기 쉽도록 하는 json 파일.
         #code는 실제로 아키텍처와 가중치가 저장되는 json 파일.
@@ -98,36 +96,60 @@ class model_control():
     def __init__(self, code):
         self.modelcode=code
         self.lines=self.modelcode.split('\n')
-        self.class_idx=[]
         self.start_class={}
-        self.class_pattern = r'class\s+(\w)\((\w)\):'
+        self.class_idx={}#각 클래스의 이름과 시작 위치
+        self.class_pattern = r'class\s+(\w+)\(nn\.Module\):'
+        self.instant_lines=[]
+        self.search_class()
     
-    def search_class(self):
-        #일단 class A(b)를 검색하여 인덱스를 얻고
-        #해당 인덱스에서 클래스 이름을 얻고
-        #얻은 클래스 이름을 바탕으로 각 클래스별 시작 지점을 찾는다.
-
+    def search_class(self):#Done.
         #이후 __init__ 내부에서 self.{name}=nn.{name}이나 custom.{name}을 통해 내부 구조를 정리한다.
         #forward 함수에서는 텐서와 레이어 단위를 기본적으로 적용하며
         #추후 custom function을 지원하여야 할 것.
-        match = re.match(self.class_pattern, self.lines[self.class_idx])
-        if match:
-            class_name = match.group(1)  # A 추출
-            base_class = match.group(2)  # B 추출
+        idx = [index for index, code in enumerate(self.lines) if code.startswith('class ')]
+        class_num=len(idx)
+        for i in range(class_num):
+            match = re.match(self.class_pattern, self.lines[idx[i]])
+            print(match)
+            class_name = match.group(1)
+            self.class_idx[class_name]=idx[i]
         #self.start_class[class_name]=
 
-    def new_architecture(self, name):
-        self.lines.append(f"\nclass {name}(nn.Module):\n")
-        self.lines.append('\tdef __init__(self):\n')
-        self.lines.append('\t\treturn 0')
-        self.lines.append(f"\t\tsuper({name}, self).__init__()\n")
-        self.lines.append('\tdef forward(self):\n')
+    def new_architecture(self, name):#Done. 기본으로 텍스트모델인지, 이미지 모델인지 설정하여 틀 만들 수 있게.
+        lens=len(self.lines)
+        self.lines.append(f"class {name}(nn.Module):")
+        self.lines.append('\tdef __init__(self):')
+        self.lines.append(f"\t\tsuper({name}, self).__init__()")
+        self.lines.append('\tdef forward(self):')
         self.lines.append('\t\tdata=0')
         self.lines.append('\t\treturn data')
+
+        self.class_idx[name]=lens
+        self.search_class()
         return 0
 
-    def modify_model(self, idx, name, code=False):#idx:모델을 추가할 위치, name:추가할 모델 명, code:가중치를 로드할지의 여부 및 그 파일명
-        #모델 아키텍처 수정.
+    def add_layer(self, tgtmod, layername, idx, arc, params):#Done. idx:모델을 추가할 위치, name:추가할 레이어 명, arc:추가할 모델명
+        #모델 레이어 추가.
+        if not arc in self.class_idx:
+            arc='nn.'+arc
+        arc=arc+f"({params})"
+        arc=f"\t\tself.{layername}={arc}"
+        self.lines.insert(self.class_idx[tgtmod]+3+idx, arc)
+        return 0
+    
+    def delete_layer(self, tgtmod, layeridx):
+        
+
+    def modify_layer(self, tgtmod, tgtidx, layername, arc, params):
+        if not arc in self.class_idx:
+            arc='nn.'+arc
+        arc=arc+f"({params})"
+        arc=f"\t\tself.{layername}={arc}"
+        self.lines[self.class_idx[tgtmod]+3+tgtidx]= arc
+        return 0
+    
+    def modify_init(self, name, params):#일단 작동은 함.
+        self.lines[self.class_idx[name]+1]=f"\tdef __init__(self, {params}):"
         return 0
 
     def modify_forward(self):
@@ -136,116 +158,225 @@ class model_control():
         #********그리고 텐서 각 모델이 어느 딕셔너리에서 어느 딕셔너리로 갈지를 지정해서 forward 함수를 쉽게 구성할 수 있다!
         return 0
 
+    def load_architecture(self, tgtmodel):
+        self.lines[self.class_idx[tgtmodel]]
+        return ''
+
+    def save(self):
+        joined_string = "\n".join(self.lines)
+        file_path = "model_architectures.py"
+        with open(file_path, "w") as file:
+            file.write(joined_string)
+
 class main_ui():
     def __init__(self):
         super(main_ui, self).__init__()
+        self.mode=''#이지, 디폴트, 커먼, 커스텀. 나중에 추가하기.
         self.models=nn.ModuleDict([])#로드한 모델 목록
         self.db=Architecture_Database()#load database
         self.control=model_control(self.db.module)
         self.visualize=False#Default
         self.cuda=torch.cuda.is_available()#Default
         self.quit=False
-        self.default_optim='Adam'
-        self.default_lr=0.0001
-        self.default_loss='MSEloss'
-        self.default_activ='ReLU'
+        self.options={}
         self.optims=[]
         self.loss_fn=[]#로스도 만들수 있게 해야 할 것.
         self.loss=[]
+        with open('option.json', 'r') as file:#옵션 로드
+            self.options = json.load(file)
 
         print('::: easy pytorch :::\n')
 
-        while not self.quit:
+        while True:
+            if self.quit:
+                break
             self.run()
+
+    def helps(self):
+        #설명서 출력
+        return 0
 
     def run(self):
         #module list나 module dict를 직접 지원할 수 있도록 추후 업데이트 해야 할 것.
 
-
         print('what do you want to do?\n')
-        print('1:load model   2:new architecture    3:option   Q:quit\n')
+        print('1:edit architecture   2:new architecture\n3:edit training loop    4:new training loop\n5:option    Q:quit\n')
         user=input('user:')
         print('\n')
 
-        if user=='Q':
+        if user=='Q':#Done.
             self.quit=True
             return 0
         
         if user=='1':
-            print(f'custom model list:{self.db.architecture_dictionary.keys()}\n')
-            if len(self.db.architecture_dictionary)==0:
-                print('No custom model found.\n')
+            
+            print('Choose model you want to edit:\n')
+            if len(self.control.class_idx)==0:
+                print('No custom architecture exists.')
                 return 0
-            user=int(input('user:'))
-            print('\n')
-            print('What do you want to do with this model?\n1:edit the model    2:train the model\n')
-            user=int(input('user:'))
-            print('\n')
-            if user=='1':
-                #모델 수정. while 문으로 돌리기. 끝나면 save할지 물어보기.
-                return 0 ##################################################
-            if user=='2':#학습시작.
-                self.train_option_setting()
-                self.training()
-                self.save()
-            return 0
+            
+            tgtmodel=input('user:')
+            while True:
+                print(self.control.load_architecture(tgtmodel)+'\n')
+                self.edit_architecture(tgtmodel)
+                self.control.save()
+                if self.quit:
+                    self.quit=False
+                    break
+
 
         if user=='2':
-            print('::: append models :::\n')
-            print('enter the name of model you want to append.\n')
-            print('enter 0 when you\'ve done.\n')
-            while user != '0':
-                user=input('Layer name:')
-                if user=='0':
-                    break
-                modelname=input('Model name:')
-                params=input('Parameters(0:Default):')
-                self.models[user]=self.new_model(modelname, params)
+            self.make_new_arch()
 
-        if user=='3':
+        if user=='5':#Done.
             self.option_setting()
             return 0
+        return 0
 
-    def train_option_setting(self):
+    def edit_architecture(self, tgtmodel):
+        print('What do you want to do?\n\n')
+        print('1:add layer    2:replace layer\n')
+        print('3:delete layer 4:edit hyper parameters\nq:quit\n')
+        user=input('user:')
+        if user=='q':
+            self.quit=True
+            return 0
+        if user=='1':
+            idx=int(input('Index to add layer:'))
+            lname=input('Layer name:')
+            arc=input('Model:')
+            params=input('Parameters:')
+            self.control.add_layer(tgtmodel, lname, idx, arc, params)
+            return 0
+        if user=='2':
+            layer=int(input('Index of the layer you want to change:'))
+            newname=input('New layer name:')
+            arc=input('Model:')
+            params=input('Parameters:')
+            self.control.modify_layer(tgtmodel, layer, newname, arc, params)
+            return 0
+        return 0
+        
+    def edit_loop(self):
+        return 0
+
+    def load_architecture(self):#아키텍처를 불러온다. 수정하기 위함. 직접적으로는 model_control에서 수정.
+        #이 함수는 그냥 유저가 보기 쉽게만 하는 용도
+        return 0
+
+    def load_model(self):#모델 인스턴스를 불러온다. 학습 혹은 테스트 위함.
+        print(f'version list:{self.db.code_dictionary.keys()}\n')
+        if len(self.db.code_dictionary)==0:
+            print('No version found.\n')
+            return 0
+        user=int(input('user:'))
+        print('\n')
+        print('What do you want to do with this model?\n1:train the model    2:test model\n')
+        user=int(input('user:'))
+        print('\n')
+        if user=='1':
+            #모델 수정. while 문으로 돌리기. 끝나면 save할지 물어보기.
+            return 0 ##################################################
+        if user=='2':#학습시작.
+            self.train_option_setting()
+            self.training()
+            self.save()
+        if user=='3':
+            self.test_model()
+        return 0
+
+    def load_loop(self):#학습 루프를 불러온다. 학습 혹은 테스트 위함.
+        return 0
+
+    def make_new_arch(self):#새 아키텍처를 만든다.
+        print('::: append models :::\n')
+        print('enter the name of model you want to append.\n')
+        print('enter 0 when you\'ve done.\n')
+        while user != '0':
+            user=input('Layer name:')
+            if user=='0':
+                break
+            modelname=input('Model name:')
+            params=input('Parameters(0:Default):')
+            self.models[user]=self.new_model(modelname, params)
+
+    def make_new_loop(self):#새 학습 루프를 만든다.
+        return 0
+
+    def train_option_setting(self):#lr을 변수로도 설정할 수 있게 하기.
         print('What do you want to do?:\n1:Add optimizer    2:Add loss function\n')
         user=input()
         if user==1:
-            print(f'What optimizer do you want to add?\n*0:default({self.default_optim})')
+            print(f'What optimizer do you want to add?\n*0:default({self.options["default_optim"]})')
             optname=input()
             if optname==0:
-                optname=self.default_optim
+                optname=self.options['default_optim']
             
             print(f'What parameters do you want to enter in this optimizer?')
-        loader=globals()[self.optimname]
+        loader=globals()[optname]
         print('What parameters do you want to enter in this optimizer?')
-        self.optims.append(loader(self.tgtmodel.parameters, lr=self.default_lr))
+        #################self.optims.append(loader(self.tgtmodel.parameters, lr=self.default_lr))
 
-    def train_loop_setting(self):
+    def train_loop_setting(self):#루프 내에 여러 루프를 포함할 수 있어야 할 것.
+        print('Do you want to load training loop?\n')
+        print('\t1:load    2:new training loop\n')
+        user=input('user:')
+        if user=='1':
+            #print(f'Train loop list:{}\n')
+            if len()==0:
+                print('No trainig loop defined.\n')
+            user=input('user:')
+            #################trainloop=globals()[user]
         return 0
     
     def training(self):
         return 0
 
+    def test_model(self):
+        return 0
+
     def start(self):
         self.setmod()
         self.set_option()
-        cycle=input('save cycle:')
-        epoch=input('train epoch:')
+        code=input('save instance as:')
+        epoch=input('train epoch(0:Default):')
         print('Batch size: default (1)')
 
-    def option_setting(self):
-        print(f"Option:\n\tdefault optimizer : {self.default_optim}\n\tdefault activate function : {self.default_activ}\n\tdefault learning rate : {self.default_lr}\n\tdefault loss function : {self.default_loss}\n")
-        user=input('What component do you want to change?\n1:defaule optimizer    2:default activate function\n3:default learning rate    4:default loss function\n5:default training epoch    6:default sacing term\n0:quit\n\nuser:')
-        if user==1:
-            self.default_optim=input('Default optim:')
-        if user==2:
-            self.default_activ=input('Default activate function:')
-        if user==3:
-            self.default_lr=input('Default learning rate:')
-        if user==4:
-            self.default_loss=input('Default loss function')
-        if user==0:
+    def option_setting(self):#Done.
+        print(f"Option:\n\tdefault optimizer : {self.options['default_optim']}\n")
+        print(f"\tdefault activate function : {self.options['default_activ']}\n")
+        print(f"\tdefault learning rate : {self.options['default_lr']}\n")
+        print(f"\tdefault loss function : {self.options['default_loss']}\n")
+        print(f"\tdefault training epoch : {self.options['default_epoch']}\n")
+        print(f"\tauto save per epoch : {self.options['auto_save']}\n")
+        print(f"\tdefault batch size:{self.options['default_batch']}\n")
+
+        print('What component do you want to change?\n')
+        print('1:defaule optimizer    2:default activate function\n')
+        print('3:default learning rate    4:default loss function\n')
+        print('5:default training epoch    6:auto save per epoch(0:disabled)\n')
+        print('7:default batch size\n')
+        print('Q:quit\n\n')
+
+        user=input('user:')
+        if user=='1':
+            self.options['default_optim']=input('Default optim:')
+        if user=='2':
+            self.options['default_activ']=input('Default activate function:')
+        if user=='3':
+            self.options['default_lr']=input('Default learning rate:')
+        if user=='4':
+            self.options['default_loss']=input('Default loss function:')
+        if user=='5':
+            self.options['default_epoch']=input('Default training epoch:')
+        if user=='6':
+            self.options['auto_save']=input('Auto save per epoch:')
+        if user=='7':
+            self.options['default_batch']=input('Default batch size:')
+        if user=='Q':
             return 0
+        with open('option.json', 'w') as file:#옵션 저장
+            json.dump(self.options, file)
         return 0
 
 main=main_ui()
